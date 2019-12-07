d7d8128a4353ff8465ac309e4493db825f68712c
KT924 <736732188@qq.com>
2019-12-07 17:10:33 +0800

    commit

--- Flask/chapter_1/app.py
+++ Flask/chapter_1/app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+import config
 app=Flask(__name__)
-
+app.config.from_object(config)
 @app.route('/')   #URL路由设置
 def index():   #对应的view函数
     return '<h3>HELLO WORLD</h3>'
-@app.route('/name/<name>')
+@app.route('/name/<name>/')
 def name(name):
     return "hello %s" % (name)
 if __name__=='__main__':
-    app.run(host='0.0.0.0')
+    app.run(host=app.config['HOST'],port=app.config['PORT'])
---
+++ Flask/chapter_1/config.py
@@ -0,0 +1,3 @@
+DEBUG="development"
+HOST="0.0.0.0"
+PORT="5001"
